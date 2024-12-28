31a26f4ef71ebf34ec7dbaac01b2c2c6e542bc39
next1code <nextcode1@gmail.com>
2024-12-28 10:08:13 +0330

    Migrations and Models done

---
+++ database/migrations/2024_09_16_145415_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            //2,147,483,647 , -2,147,483,647
+            $table->string('mobile')->nullable();
+            //09142558201
+            $table->string('postal_code')->nullable();
+            //5714644936
+            $table->text('address');
+            $table->foreignId('country_id')->constrained();
+            $table->foreignId('state_id')->constrained();
+            $table->foreignId('city_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
