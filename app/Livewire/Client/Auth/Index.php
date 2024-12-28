65597c8a3734b843539808f0838f0baf26c2ca4d
next1code <nextcode1@gmail.com>
2024-12-29 00:18:52 +0330

    Create auth with Google

--- app/Livewire/Client/Auth/Index.php
+++ app/Livewire/Client/Auth/Index.php
@@ -2,10 +2,22 @@
 
 namespace App\Livewire\Client\Auth;
 
+use Laravel\Socialite\Facades\Socialite;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $gmailUser = Socialite::driver('google')->stateless()->user();
+        dd($gmailUser);
+    }
+
     public function render()
     {
         return view('livewire.client.auth.index')->layout('layouts.client.app');
--- routes/client.php
+++ routes/client.php
@@ -7,5 +7,7 @@ use App\Livewire\Client\Auth\Index as AuthIndex;
 Route::name('client.')->group(function () {
 
     Route::get('/auth', AuthIndex::class)->name('auth');
+    Route::get('/gmail', [AuthIndex::class,'redirectToProvider'])->name('gmail');
+    Route::get('/auth/gmail/callback', [AuthIndex::class,'handleProviderCallback'])->name('gmail.callback');
 
 });
