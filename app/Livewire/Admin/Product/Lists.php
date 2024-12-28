6916e153c8e4291b5501560935159d29d92e602b
next1code <nextcode1@gmail.com>
2024-12-28 20:56:29 +0330

    Delete products in admin panel

--- app/Livewire/Admin/Product/Lists.php
+++ app/Livewire/Admin/Product/Lists.php
@@ -3,12 +3,27 @@
 namespace App\Livewire\Admin\Product;
 
 use App\Models\product;
+use App\Models\ProductImage;
+use App\Models\SeoItems;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class Lists extends Component
 {
     use WithPagination;
+
+    public function delete($productId)
+    {
+        DB::transaction(function () use ($productId) {
+            product::query()->where('id', $productId)->delete();
+            ProductImage::query()->where('product_id', $productId)->delete();
+            SeoItems::query()->where('ref_id', $productId)->delete();
+            File::deleteDirectory('products/' . $productId);
+
+        });
+    }
 
     public function render()
     {
