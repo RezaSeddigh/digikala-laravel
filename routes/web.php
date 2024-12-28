e0a5cb1c461e624bd5f7ef8d17bd4423ca0a6a44
next1code <nextcode1@gmail.com>
2024-12-28 10:17:03 +0330

    The main structure for admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('admin')->group(function () {
+    require __DIR__ . '/admin.php';
+
+
+});
+
+require __DIR__ . '/client.php';
+
+
+Route::prefix('seller')->group(function () {
+    require __DIR__ . '/seller.php';
 });
